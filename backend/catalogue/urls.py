9034a796eec610406b8077515d115290d3a3b4fa
Krystof Kopecny <krystof.kope@gmail.com>
2022-07-30 00:15:54 +0200

    url edit

--- backend/catalogue/urls.py
+++ backend/catalogue/urls.py
@@ -3,8 +3,10 @@ from django.urls import path, include
 from .views import (
     PythonTopicsListView,
     create_python_topic_view,
-    PythonTopicsDetailView,
     update_python_topic_view,
+    python_topic_detail_view,
+    python_topic_random_detail_view,
+
     )
 
 
@@ -12,7 +14,8 @@ urlpatterns = [
     path('topics/', include([
         path('', PythonTopicsListView.as_view(), name="topics-list"),
         path('create/', create_python_topic_view, name="create-topic"),
-        path('<int:pk>/detail', PythonTopicsDetailView.as_view(), name="topic-detail"),
-        path('<int:pk>/update', update_python_topic_view, name="topic-update"),
+        path('<int:pk>/detail/', python_topic_detail_view, name="topic-detail"),
+        path('<int:pk>/update/', update_python_topic_view, name="topic-update"),
+        path("random_topic", python_topic_random_detail_view, name="topic-random")
     ])),
 ]
