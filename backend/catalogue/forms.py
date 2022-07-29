299b08d50d8de10ba2bd85c24d8a7aae244467e3
Krystof Kopecny <krystof.kope@gmail.com>
2022-07-30 00:15:38 +0200

    PythonTopi form created

--- backend/catalogue/forms.py
+++ backend/catalogue/forms.py
@@ -1,6 +1,8 @@
-from dataclasses import fields
 from django.forms import ModelForm
 from .models import PythonTopic
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
 
 class PythonTopicForm(ModelForm):
     class Meta:
@@ -10,3 +12,13 @@ class PythonTopicForm(ModelForm):
             "describtion",
             "python_file",
         ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            "topic_name",
+            "describtion",
+            "python_file",
+            Submit('submit', 'Submit', css_class='button white'),
+        )
