526ef43180ba5fac7e16448376cbf290982be13a
Krystof Kopecny <krystof.kope@gmail.com>
2022-07-31 22:16:56 +0200

    PythonTopic model registered

--- backend/catalogue/admin.py
+++ backend/catalogue/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import PythonTopic
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "id", "is_active")
+
+
+class PythonTopicAdmin(admin.ModelAdmin):
+    list_display = ("topic_name", "created", "user", "id")
+admin.site.register(PythonTopic, PythonTopicAdmin)
+
+
