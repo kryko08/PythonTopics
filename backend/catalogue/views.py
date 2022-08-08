1a9751fb9e33c47b44df9b7c87ae0ca9bd112688
Krystof Kopecny <krystof.kope@gmail.com>
2022-08-08 16:59:31 +0200

    update view fixed

--- backend/catalogue/views.py
+++ backend/catalogue/views.py
@@ -4,6 +4,8 @@ from django.http import Http404, HttpResponseRedirect
 from django.views.generic import (
     ListView,
 )
+from django.core.exceptions import PermissionDenied
+
 
 from .models import PythonTopic
 from .forms import PythonTopicForm, CustomUserRegistrationForm
@@ -11,6 +13,7 @@ from .forms import PythonTopicForm, CustomUserRegistrationForm
 from Topics.settings import MEDIA_ROOT
 
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
 import os
 import sys
@@ -19,6 +22,7 @@ import inspect
 import subprocess
 from random import randint
 from ast import mod
+import operator
 
 
 class PythonTopicsListView(ListView):
@@ -27,6 +31,7 @@ class PythonTopicsListView(ListView):
     template_name = "catalogue/topics_list.html"
 
 
+@login_required()
 def create_python_topic_view(request):
     if request.method == 'POST':
         form = PythonTopicForm(request.POST, request.FILES)
@@ -51,25 +56,40 @@ def create_python_topic_view(request):
         )
 
 
-# def update_python_topic_view(request, pk=None):
-#     topic = get_object_or_404(PythonTopic, pk)
-#     if request.method == 'POST':
-#         form = PythonTopicForm(request.POST, instance=topic)
-#         if form.is_valid():
-#             topic.save()
-#             return redirect("catalogue/topics_list.html")
+@login_required()
+def update_python_topic_view(request, pk=None):
+    # Check if user is owner or admin (admin can edit any posts) 
+    topic = get_object_or_404(PythonTopic, pk=pk)
+    if request.user != topic.user and not request.user.is_superuser:
+            raise PermissionDenied
 
-#     else:
-#         form = PythonTopicForm(instance=topic)
+    # Handle update
+    if request.method == 'POST':
+        form = PythonTopicForm(request.POST, request.FILES, instance=topic)
+        if form.is_valid():
+            file = form.cleaned_data["python_file"]
+            topic_name = form.cleaned_data["topic_name"]
+            describtion = form.cleaned_data["describtion"]
+            # Override data
+            # PythonTopic.objects.filter(pk=pk).update(topic_name=topic_name, describtion=describtion, python_file=file)
+            topic.topic_name = topic_name
+            topic.python_file = file
+            topic.describtion = describtion
+            topic.save()
+            return HttpResponseRedirect(
+                reverse('topic-detail', kwargs={'pk': topic.id}))
+    else:
+        form = PythonTopicForm(instance=topic)
 
-#     return render(
-#         request,
-#         'catalogue/update_topic.html',
-#         {"form": form,
-#         "topic": topic}
-#         )
+    return render(
+        request,
+        'catalogue/update_topic.html',
+        {"form": form,
+        "topic": topic}
+        )
 
 
+@login_required()
 def python_topic_detail_view(request, pk=None):
     # Grab detail object
     python_topic = get_object_or_404(PythonTopic, pk=pk)
