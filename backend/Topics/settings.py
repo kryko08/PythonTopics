ec6fa8784be62d42452874909c6387844c162200
Krystof Kopecny <krystof.kope@gmail.com>
2022-08-17 17:21:36 +0200

    test media root added

--- backend/Topics/settings.py
+++ backend/Topics/settings.py
@@ -135,6 +135,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+TEST_MEDIA_ROOT = os.path.join(BASE_DIR, "test_media")
 
 LOGIN_REDIRECT_URL = "/topics"
 LOGOUT_REDIRECT_URL = "/topics"
