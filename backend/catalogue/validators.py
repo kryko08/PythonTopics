07c35cd95c7d3ed3596730644a8b94b83c0dc6f9
Krystof Kopecny <krystof.kope@gmail.com>
2022-08-17 17:21:11 +0200

    validator fixed

--- backend/catalogue/validators.py
+++ backend/catalogue/validators.py
@@ -3,13 +3,8 @@ from django.core.exceptions import ValidationError
 def is_python_file_validator(fieldfile):
     import os
     filename, user_input = os.path.splitext(fieldfile.name)
-    error_message = "File format invalid! Expected %(expected_file_format)s. Got instead %(got_instead)s"
     expected_filetype = ".py"
-    error_dict = {
-        "expected_file_format": expected_filetype,
-        "got_instead": user_input
-    }
-
+    error_message = f"File format invalid! Expected {expected_filetype}. Got instead {user_input}"
     if user_input != expected_filetype:
-        raise ValidationError(error_message, error_dict)
+        raise ValidationError(error_message)
         
